a666cf5293e39c126eb715259005668c8fb6a9a3
CristianLopez72 <114121820+CristianLopez72@users.noreply.github.com>
2023-03-08 23:34:08 -0300

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+//Importo Librerias a utilizar
+import Router from "koa-router";
+import ProductRouter from "./products.routes.js";
+
+const router = new Router({
+  prefix: "/api",
+});
+
+router.use(ProductRouter);
+
+router.get("/", (ctx) => {
+  console.log(ctx);
+});
+
+export default router.routes();
